1da84b7966285ee9925db983b6eddf2dff2c772a
Garv Shah <garvshah01@gmail.com>
2021-07-25 09:57:34 +0530

    Update in chat section

--- lib/chat.dart
+++ lib/chat.dart
@@ -20,75 +20,92 @@ class ChatPage extends StatelessWidget {
             )
           ],
         ),
-        body: Column(children: [
-          Container(
+        body: Container(
+            decoration: new BoxDecoration(color: Color(0xffffb38a)),
+            child: Column(children: [
+              Container(
 
-              //
-              width: screensize.width,
-              child: Card(
-                  margin: EdgeInsets.all(8),
-                  elevation: 4.0,
-                  child: Padding(
-                    padding: EdgeInsets.all(10),
-                    child: Text("Questions People have asked AstroBuddy:",
-                        style: TextStyle(
-                          fontSize: 20,
-                        )),
-                  ))),
-          Container(
-              color: Colors.orange[200],
-              margin: EdgeInsets.only(top: 10),
-              child: ListView(shrinkWrap: true, children: [
-                ListTile(
-                    leading: Icon(
-                      Icons.book,
-                      color: Colors.orange,
-                      size: 50,
+                  //
+                  width: screensize.width,
+                  child: Card(
+                      margin: EdgeInsets.all(8),
+                      elevation: 4.0,
+                      child: Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text("Questions People have asked AstroBuddy:",
+                            style: TextStyle(
+                              fontSize: 20,
+                            )),
+                      ))),
+              Container(
+                  // color: Colors.orange[200],
+                  margin: EdgeInsets.only(top: 10),
+                  child: ListView(shrinkWrap: true, children: [
+                    ListTile(
+                        leading: Icon(
+                          Icons.book,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        title: Card(
+                            color: Colors.white.withOpacity(0.2),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                  "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
+                                  style: TextStyle(fontSize: 18)),
+                            ))),
+                    Divider(
+                      thickness: 0,
+                      color: Colors.transparent,
                     ),
-                    title: Card(
-                        color: Colors.white.withOpacity(0.1),
-                        elevation: 0.0,
-                        child: Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text(
-                              "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
-                              style: TextStyle(fontSize: 18)),
-                        ))),
-                Divider(
-                  thickness: 0,
-                  color: Colors.transparent,
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.book,
-                    color: Colors.orange,
-                    size: 50,
-                  ),
-                  title: Text(
-                      "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
-                      style: TextStyle(fontSize: 18)),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.book,
-                    color: Colors.orange,
-                    size: 50,
-                  ),
-                  title: Text(
-                      "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
-                      style: TextStyle(fontSize: 18)),
-                ),
-                ListTile(
-                  leading: Icon(
-                    Icons.book,
-                    color: Colors.orange,
-                    size: 50,
-                  ),
-                  title: Text(
-                      "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
-                      style: TextStyle(fontSize: 18)),
-                )
-              ]))
-        ]));
+                    ListTile(
+                        leading: Icon(
+                          Icons.book,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        title: Card(
+                            color: Colors.white.withOpacity(0.1),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                  "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
+                                  style: TextStyle(fontSize: 18)),
+                            ))),
+                    ListTile(
+                        leading: Icon(
+                          Icons.book,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        title: Card(
+                            color: Colors.white.withOpacity(0.1),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                  "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
+                                  style: TextStyle(fontSize: 18)),
+                            ))),
+                    ListTile(
+                        leading: Icon(
+                          Icons.book,
+                          color: Colors.orange,
+                          size: 50,
+                        ),
+                        title: Card(
+                            color: Colors.white.withOpacity(0.1),
+                            elevation: 0.0,
+                            child: Padding(
+                              padding: EdgeInsets.all(10),
+                              child: Text(
+                                  "I am not so good at studies, I would like to know whether I should go for higher studies or join family business",
+                                  style: TextStyle(fontSize: 18)),
+                            )))
+                  ]))
+            ])));
   }
 }
